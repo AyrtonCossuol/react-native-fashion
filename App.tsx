36701ba0f4553cb511709803eb3dd053940bcf22
AyrtonCossuol <ayrton.cossuol@live.com>
2020-07-21 10:19:53 -0300

    creating the basic stylizations

--- App.tsx
+++ App.tsx
@@ -1,13 +1,19 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Onboarding from './src/Authentication/Onboarding';
+import { LoadAssets } from './src/components';
+
+const fonts = {
+  "SFProText-Bold": require("./assets/fonts/SF-Pro-Text-Bold.otf"),
+  "SFProText-Semibold": require("./assets/fonts/SF-Pro-Text-Semibold.otf"),
+  "SFProText-Regular": require("./assets/fonts/SF-Pro-Text-Regular.otf"),
+};
 
 const AuthenticationStack = createStackNavigator();
 const AuthenticationNavigator = () => {
   return(
-    <AuthenticationStack.Navigator>
+    <AuthenticationStack.Navigator headerMode="none">
       <AuthenticationStack.Screen name='Onboarding' component={Onboarding} />
     </AuthenticationStack.Navigator>
   );
@@ -15,8 +21,8 @@ const AuthenticationNavigator = () => {
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <LoadAssets {...{ fonts }}>
       <AuthenticationNavigator />
-    </NavigationContainer>
+    </LoadAssets>
   );
 }
