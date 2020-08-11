29d1719a180fee6fdef20839c3e17343627117a4
AyrtonCossuol <ayrton.cossuol@live.com>
2020-08-11 10:49:09 -0300

    finalizing the login page and building the registration page

---
+++ src/Authentication/SignUp.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+interface SignUp {
+
+}
+
+const SignUp = () => {
+    return (
+        <View />
+    );
+}
+
+export default SignUp;
--- src/Authentication/components/Form/TextInput.tsx
+++ src/Authentication/components/Form/TextInput.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import { TextInput as RNTextInput, StyleSheet, TextInputProps as RNTextInputProps } from 'react-native';
 
 import { Feather as Icon } from '@expo/vector-icons';
@@ -11,7 +11,8 @@ interface TextInputProps extends RNTextInputProps {
     error?: string;
 };
 
-const TextInput = ({ icon, touched, error, ...props }: TextInputProps) => {
+const TextInput = forwardRef(
+    ({ icon, touched, error, ...props }: TextInputProps, ref) => {
     const theme = useTheme();
     const SIZE = theme.borderRadii.m * 2;
     const reColor = !touched ? 'text' : error ? 'danger' : 'primary';
@@ -36,6 +37,7 @@ const TextInput = ({ icon, touched, error, ...props }: TextInputProps) => {
                     underlineColorAndroid='transparent' 
                     placeholderTextColor={color} 
                     {...props}
+                    {...ref}
                 />
             </Box>
             {
@@ -47,13 +49,18 @@ const TextInput = ({ icon, touched, error, ...props }: TextInputProps) => {
                         backgroundColor={!error ? 'primary' : 'danger'}
                         justifyContent='center'
                         alignItems='center'
+                        style={{ borderRadius: SIZE / 2 }}
                     >
-                        <Icon name={!error ? 'check' : 'x'} size={16} color='white' />
+                        <Icon 
+                            name={!error ? 'check' : 'x'} 
+                            size={16} color='white' 
+                            style={{ textAlign: 'center' }}
+                        />
                     </Box> 
                 )
             }
         </Box>
     );
-};
+});
 
 export default TextInput;
